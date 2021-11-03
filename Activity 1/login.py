03a544592373440c10816d2af140bf941fd8500f
Blackwood <68885343+Weenut@users.noreply.github.com>
2021-11-02 19:54:31 -0500

    /
    Using Python, create the following algorithm:
    
    - Create a variable called "password_log" and assign it the value of "bigfoot".
    - Accept a user's name and saves it to a variable called "username".
    - Accept a user's password and saves it to a variable called "password".
    - Using an if statement, check if the value that the user has entered for their password is the same as the value stored in password_log (i.e. password = password_log)
    - If the passwords match, print the following message: "User [username] has successfully logged in."
    - If the passwords don't match, print the following message: "Incorrect password entered. Please restart the program and try again."

---
+++ Activity 1/login.py
@@ -0,0 +1,10 @@
+# variables
+password_log = "bigfoot"
+username = input(str("Enter yer username kiddo"))
+password = input(str("Enter yo password son"))
+
+# checking the password
+if password == password_log:
+  print("User [username] has successfully logged in.")
+elif password != password_log:
+  print("Incorrect password entered. Please restart the program and try again.")
