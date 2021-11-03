f40276fd44ae9c1da42c4ff713a7ccaaa71e3088
Weenut <68885343+Weenut@users.noreply.github.com>
2021-11-03 09:15:14 -0500

    fix jank

--- Activity 1/login.cpp
+++ Activity 1/login.cpp
@@ -4,16 +4,16 @@
 int main() {
   string password_log = "bigfoot", password, username;
   
-  cout << "Enter yer username kiddo" << endl;
+  cout << "Enter your username\n";
   cin >> username;
-  cout << "Enter yo password son" << endl;
+  cout << "Enter your password\n";
   cin >> password;
   
   if (password == password_log) {
-    cout << "User " , username, " has successfully logged in." << endl;
+    cout << "User " << username << " has successfully logged in." << endl;
   }
   else if (password != password_log) {
-    cout << "Incorrect password entered. Please restart the program and try again." << endl;
+    cout << "Incorrect password entered. Please restart the program and try again."
   }
   return 0;
 }
