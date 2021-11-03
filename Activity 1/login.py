6a89538367b8d1a6e465433de8f9e5e0033f39d5
Blackwood <68885343+Weenut@users.noreply.github.com>
2021-11-03 09:00:01 -0500

    Update login.py

--- Activity 1/login.py
+++ Activity 1/login.py
@@ -5,6 +5,6 @@ password = input(str("Enter yo password son"))
 
 # checking the password
 if password == password_log:
-  print("User [username] has successfully logged in.")
+  print("User", username, "has successfully logged in.")
 elif password != password_log:
   print("Incorrect password entered. Please restart the program and try again.")
