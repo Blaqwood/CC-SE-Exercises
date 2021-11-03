8244804ceec78fe45b9a50bfc5ee10f12b8b9704
Blackwood <68885343+Weenut@users.noreply.github.com>
2021-11-02 20:07:32 -0500

    Update login.cpp

--- Activity 1/login.cpp
+++ Activity 1/login.cpp
@@ -1,7 +1,15 @@
 #include <string>
 #include <iostream>
 
+/*
+before you say it
+yes I know this line below is bad
+I'm just lazy and this is a smol program anyway
+*/
+using namespace std;
+
 int main() {
+  // declare variables
   string password_log = "bigfoot", password, username;
   
   cout << "Enter yer username kiddo" << endl;
@@ -9,8 +17,9 @@ int main() {
   cout << "Enter yo password son" << endl;
   cin >> password;
   
+  // verify password
   if (password == password_log) {
-    cout << "User " , username, " has successfully logged in." << endl;
+    cout << "User " << username << " has successfully logged in." << endl;
   }
   else if (password != password_log) {
     cout << "Incorrect password entered. Please restart the program and try again." << endl;
