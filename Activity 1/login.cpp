c35a4add3873524023e74102948b6f235c8b2dbb
Weenut <blackwoodj3311@gmail.com>
2021-11-28 22:10:33 +0000

    updated login.cpp to use classes

--- Activity 1/login.cpp
+++ Activity 1/login.cpp
@@ -1,19 +1,34 @@
+/*
+  A program that check the password entered and logs you in
+*/
+
 #include <string>
 #include <iostream>
 
-int main() {
-  std::string password_log = "bigfoot", password, username;
-  
-  std::cout << "Enter your username" << std::endl;
-  std::cin >> username;
-  std::cout << "Enter your password" << std::endl;
-  std::cin >> password;
-  
-  if (password == password_log) {
-    std::cout << "User " << username << " has successfully logged in." << std::endl;
+class Auth {
+  public:
+    void passwordcheck(std::string uname, std::string passwd);
+  private:
+    std::string password_log = "bigfoot";
+};
+
+//check the password by comparing it to password_log
+void Auth::passwordcheck(std::string uname, std::string passwd) {
+  if (passwd == password_log) {
+    std::cout << "User " << uname << " has successfully logged in." << std::endl;
   }
-  else if (password != password_log) {
+  else if (passwd != password_log) {
     std::cout << "Incorrect password entered. Please restart the program and try again.";
   }
+}
+
+int main() {
+  std::string u_auth, p_auth;
+  std::cout << "Enter your username" << std::endl;
+  std::cin >> u_auth;
+  std::cout << "Enter your password" << std::endl;
+  std::cin >> p_auth;
+  Auth logon;
+  logon.passwordcheck(u_auth, p_auth);
   return 0;
 }
